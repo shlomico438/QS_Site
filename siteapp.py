e78daa62576487dbd4b42a505b239dd8ce6ddabc
shlomico438 <shlomico1234@gmail.com>
2026-01-14 19:23:02 +0200

    Added user connections

--- siteapp.py
+++ siteapp.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os
+import boto3
 
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+    region_name='us-east-1' # Make sure this matches your bucket region
+)
 app = Flask(__name__)
 
 # --- Settings ---
